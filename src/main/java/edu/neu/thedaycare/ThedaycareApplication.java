d2f128427a88d0f7df8f3f5d9c90a2e96ceaeda7
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-04-28 00:20:26 -0400

    cleanup links

--- src/main/java/edu/neu/thedaycare/ThedaycareApplication.java
+++ src/main/java/edu/neu/thedaycare/ThedaycareApplication.java
@@ -13,10 +13,5 @@ public class ThedaycareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThedaycareApplication.class, args);
 	}
-	
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}	
 
 }
