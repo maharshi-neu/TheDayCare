b8da2f6a0e8d619cc4a5407bced2f785ef8c8c67
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-04-28 10:32:22 -0400

    config autowired not working

--- src/main/java/edu/neu/thedaycare/EmailService.java
+++ src/main/java/edu/neu/thedaycare/EmailService.java
@@ -1,6 +1,5 @@
 package edu.neu.thedaycare;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -10,22 +9,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-//	@Autowired
-//	private EmailConfiguration emailConfiguration;
-
 	public JavaMailSender getJavaMailSender() {
-//		EmailConfiguration emailConfiguration = new EmailConfiguration();
 		JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
-//		mailSenderImpl.setHost(emailConfiguration.getHost());
-//		mailSenderImpl.setPort(emailConfiguration.getPort());
-//		mailSenderImpl.setUsername(emailConfiguration.getUsername());
-//		mailSenderImpl.setPassword(emailConfiguration.getPassword());
 		
-		mailSenderImpl.setHost("smtp.mailtrap.io");
-		mailSenderImpl.setPort(2525);
-		mailSenderImpl.setUsername("9d82be49777855");
-		mailSenderImpl.setPassword("a70b78c24381c7");
-		
+		mailSenderImpl.setHost(EmailConfiguration.host);
+		mailSenderImpl.setPort(EmailConfiguration.port);
+		mailSenderImpl.setUsername(EmailConfiguration.username);
+		mailSenderImpl.setPassword(EmailConfiguration.password);
 		
 		return mailSenderImpl;
 	}
